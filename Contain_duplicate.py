017b135ea17e9498aa27a71d0df31692e4212af8
alaminsheikh01 <alaminsheikh9746@gmail.com>
2026-05-24 23:43:19 +0200

    duplicate contain in different way added

--- Contain_duplicate.py
+++ Contain_duplicate.py
@@ -9,3 +9,20 @@ def Solution(nums):
 
 nums = [1,2,3,1]
 print(Solution(nums))
+
+
+# Different way
+
+def Solution(nums):
+
+    seen = set()
+
+    for num in nums:
+        if num in seen:
+            return True
+        seen.add(num)
+    return False        
+
+
+nums = [1,2,3,4,1]
+print(Solution(nums))
--- Practice.py
+++
@@ -1,4 +0,0 @@
-a = ["red", "green", "blue"]
-
-for i, val in enumerate(a, 1):
-    print(i, val)
