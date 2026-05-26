a58db0e3d5f2b2d6ba5f6c80a53ae26b6f3e7b2b
alaminsheikh01 <alaminsheikh9746@gmail.com>
2026-05-26 17:41:37 +0200

    Length of last word

---
+++ Length_of_last_word.py
@@ -0,0 +1,15 @@
+s = "Hello World"
+
+# start and end empty space will be remove
+s = s.strip()
+
+# setence will be inside a list [...]
+word = s.split()
+
+# get the last item from the list
+s = word[-1]
+
+# find len
+s = len(s)
+
+print(s)
